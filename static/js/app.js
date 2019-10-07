6fcaaddafb281e7445ff952dc488a9a22e6fbbbc
carsonlepre <carson@scoot.co>
2019-10-06 18:31:02 -0700

    fixed issue with repopulating table

--- static/js/app.js
+++ static/js/app.js
@@ -3,27 +3,27 @@ var tableData = data;
 var table = document.getElementById("ufo-table");
 var button = document.getElementById("filter-btn");
 
-tableData.forEach(tableList);
+tableData.forEach(tableInsert);
 // function connected to the query from the index.html page. 
 /*(The dataset actually only has one country, so I removed the "country" field from the starter html, so users wouldn't 
 have the opportunity to input improperly formatted country queries. */
 function returnQuery() {
 
-  var dateFilter = document.getElementById("datetime").value;
-  var cityFilter = document.getElementById("city").value;
-  var stateFilter = document.getElementById("state").value;
-  var shapeFilter = document.getElementById("shape").value;
+  var date = document.getElementById("datetime").value;
+  var city = document.getElementById("city").value;
+  var state = document.getElementById("state").value;
+  var state = document.getElementById("shape").value;
 
   /*if/else block checks if anything has been input into the search fields, filters if that's what's needed, returns the full
   table if search fields are blank */
-  if (dateFilter.length !== 0 || cityFilter.length !== 0 || stateFilter.length !== 0 || shapeFilter.length !== 0) {
+  if (date.length !== 0 || city.length !== 0 || state.length !== 0 || state.length !== 0) {
     var filterData = tableData.filter(function (ufo) 
     {
         return (
-        (ufo.datetime === dateFilter || dateFilter.length === 0) &&
-        (ufo.city === cityFilter || cityFilter.length === 0) &&
-        (ufo.state === stateFilter || stateFilter.length === 0) &&
-        (ufo.shape === shapeFilter || shapeFilter.length === 0)
+        (ufo.datetime === date || date.length === 0) &&
+        (ufo.city === city || city.length === 0) &&
+        (ufo.state === state || state.length === 0) &&
+        (ufo.shape === state || state.length === 0)
         );
       });
     } 
@@ -36,16 +36,17 @@ function returnQuery() {
       table.deleteRow(i);
      }
   
-  filterData.forEach(tableList);
-
+  filterData.forEach(tableInsert);
 }
 //This function just lets users click back to the full table.
 function repopulatePage() {
   var table = document.getElementById("ufo-table");
-  tableData.forEach(tableList);
+  table.innerHTML=""
+  tableData.forEach(tableInsert);
 }  
 
-function tableList(ufo) {
+//function for populating the table
+function tableInsert(ufo) {
 
     var row = table.insertRow(-1);
 
